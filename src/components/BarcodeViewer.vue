26072ffcd165a25af6e120916369d77cc6c85820
Spiridonov Maxim <taijased@gmail.com>
2019-11-12 16:20:23 +0300

    create temp screen loader view

--- src/components/BarcodeViewer.vue
+++ src/components/BarcodeViewer.vue
@@ -1,25 +1,35 @@
-<template >
-    <div class="viewer">
-        <a-scene embedded vr-mode-ui="enabled: false" arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;">
-        <a-assets>
-            <a-asset-item id="animated-asset" src="https://raw.githubusercontent.com/nicolocarpignoli/nicolocarpignoli.github.io/master/ar-playground/models/CesiumMan.gltf"></a-asset-item>
-        </a-assets>
-        <a-marker-camera type='pattern' url='https://arq.su/pattern-mark.patt'>
-            <a-entity
-                animation-mixer
-                gltf-model="#animated-asset"
-                scale="2 2 2">
-            </a-entity>
-        </a-marker-camera>
-        <a-entity camera></a-entity>
-        </a-scene>
-    </div>
+<template lang="pug">
+    .viewer
+        a-scene(v-if='visible', embedded='', vr-mode-ui='enabled: false', arjs='sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;')
+            a-assets
+                a-asset-item#animated-asset(src='https://raw.githubusercontent.com/nicolocarpignoli/nicolocarpignoli.github.io/master/ar-playground/models/CesiumMan.gltf')
+                a-marker-camera(type='pattern', url='https://arq.su/pattern-mark.patt')
+                    a-entity(animation-mixer='', gltf-model='#animated-asset', scale='2 2 2')
+                a-entity(camera='')
+        .au-media(v-else)
+            img(src="../assets/viewer/aumedia.svg")
+            img(src="../assets/viewer/close.svg")
+            img(src="../assets/viewer/arq-family.svg")
+            .custom-progress
+                span
+            span Подождите, загружаем
+
+
 
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            visible: false
+        }
+    },
+    beforeCreate() {
+        setTimeout(() => {
+            this.visible = true
+        }, 3000);
+    }
 }
 </script>
 
@@ -28,6 +38,54 @@ export default {
     width 100vw
     height 100vh
     overflow hidden
+
+    .au-media
+        width 100%
+        height 100%
+        display flex
+        flex-direction column
+        justify-content center
+        align-items center
+
+        span 
+
+            font-family: 'TT Norms Medium';
+            font-style: normal;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 19px;
+            /* identical to box height */
+            text-align: center;
+            color: #000000;
+        .custom-progress
+            width 280px
+            height 20px
+            border: 2px solid #4737D6;
+            box-sizing: border-box;
+            margin 16px 0
+            border-radius: 10px;
+            display flex
+            flex-direction row
+            justify-content flex-start
+            align-items center
+            span
+                margin 0 8px
+                width 1px
+                height 8px
+                background: #4737D6;
+                border-radius: 10px;
+                animation-duration: 3s;
+                animation-name: slidein;
+
+
+@keyframes slidein {
+  from {
+    width: 1px; 
+  }
+  to {
+    width: 263px;
+  }
+}
 
 
 </style>
